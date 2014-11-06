4fb7858998e87857a5370968bba3a5a689769b5c
chunjunghee <dkddkd0311@naver.com>
2014-11-06 14:46:21 +0900

    new

--- sketch.js
+++ sketch.js
@@ -1,18 +1,19 @@
 function setup() 
 {
    createCanvas(800,800);
+   bg = loadImage("galaxy.jpg");
 }
 
 function draw() 
 {
-	background(255);
-	noStroke();
-	fill(249,42,8);
+   background(bg);
+   noStroke();
+   fill(249,42,8);
     ellipse(400,400,150,150);
 
 //earth
-	var duration = 3000;
-	var timing = (new Date()%duration)/duration;
+   var duration = 3000;
+   var timing = (new Date()%duration)/duration;
 
    fill(27,105,109);
    ellipse(400 + Math.cos(timing*2*PI)*200,           // x좌표
@@ -21,18 +22,18 @@ function draw()
         100);            
 
 //moon
-	var duration = 1000;
-	var timing2 = (new Date()%duration)/duration;
+   var duration = 1000;
+   var timing2 = (new Date()%duration)/duration;
 
    fill(255,206,0);
    ellipse(400 + Math.cos(timing*2*PI)*200+ Math.cos(timing2*2*PI)*70,           // x좌표
-        400 + Math.sin(timing*2*PI)*200+ Math.(timing2*2*PI)*70, // y좌표
+        400 + Math.sin(timing*2*PI)*200+ Math.sin(timing2*2*PI)*70, // y좌표
         30,                                  // width
         30);   
                                       // height
 
     var duration = 3000;
-	var timing = (new Date()%duration)/duration;
+   var timing = (new Date()%duration)/duration;
 
    fill(190,240,8);
    ellipse(400 + Math.cos(timing*2*PI)*300+ Math.cos(timing*2*PI)*70,           // x좌표
